483622c8b7d2546d4e3ac7113faf2c368882edf6
Unni <unnisathya88@gmail.com>
2016-05-29 13:10:06 +0400

    Add Comment

--- RR-backup.sh
+++ RR-backup.sh
@@ -6,4 +6,5 @@ DATE=`date +%d-%m-%Y`
 
 /usr/local/bin/aws s3 sync zonefiles/ s3://my.domain.backup/  --region us-west-1
 
+#Clear files that are >30 days old.
 /usr/bin/find /home/route53/zonefiles/ -mtime +30 -exec rm  {} \;
