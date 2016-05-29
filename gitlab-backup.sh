c741c5fe5af1251ade87cebdb90ab519603cb771
Unni <unnisathya88@gmail.com>
2016-05-29 14:02:26 +0400

    Add Gitlab Backup

---
+++ gitlab-backup.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+#Command to initiate backup
+/usr/bin/gitlab-rake gitlab:backup:create
+
+now_date=`date +%d-%m-%Y`
+
+cp /etc/gitlab/gitlab-secrets.json /var/opt/gitlab/backups/
+
+#Rename with date appended
+mv /var/opt/gitlab/backups/gitlab-secrets.json /var/opt/gitlab/backups/gitlab-secrets.json.$now_date
+
+/usr/bin/aws s3 sync /var/opt/gitlab/backups/ s3://aa.gitlab.backup/ --region eu-west-1
+
+#Deleting files older than 7 days.
+/usr/bin/find /var/opt/gitlab/backups/ -mtime +7 -exec rm  {} \;
+
+
