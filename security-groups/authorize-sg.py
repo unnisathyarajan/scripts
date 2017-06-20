a71fba37bd41c0a4e9b2f90fbb1196a3d6519f16
unnisathya <unnisathya88@gmail.com>
2017-06-20 12:24:56 +0400

    changed file handle code and removed commented lines.

---
+++ security-groups/authorize-sg.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python 
+__author__ = "unnisathya"
+
+import argparse
+import boto3
+from  urllib2 import urlopen
+
+
+def main():
+ parser = argparse.ArgumentParser()
+ parser.add_argument("security_group",help="Example: sg-28f3ed4c")
+ parser.add_argument("protocol_number",help="Example: 22")
+ parser.add_argument("ip",help="Example: 192.168.1.1/32")
+ parser.add_argument("region",help="Example: us-east-1")
+
+ args = parser.parse_args()
+
+ ec2 = boto3.resource('ec2', region_name=args.region)
+ security_group = ec2.SecurityGroup(args.security_group)
+ security_group.authorize_ingress(IpProtocol="tcp",FromPort=int(args.protocol_number),ToPort=int(args.protocol_number),CidrIp=args.ip)
+
+ with open("IPList.txt",'a') as Tmpfile:
+  Tmpfile.write(args.ip + '\n')
+
+
+
+if __name__ == '__main__':
+
+ main()
+
