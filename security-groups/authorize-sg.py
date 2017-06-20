f7a480eba2ff7e7cceecc7c5ca0cd10c37cb4c7b
unnisathya <unnisathya88@gmail.com>
2017-06-20 14:11:51 +0400

    name changed protocol to port

--- security-groups/authorize-sg.py
+++ security-groups/authorize-sg.py
@@ -8,7 +8,7 @@ import boto3
 def main():
  parser = argparse.ArgumentParser()
  parser.add_argument("security_group",help="Example: sg-28f3ed4c")
- parser.add_argument("protocol_number",help="Example: 22")
+ parser.add_argument("port_number",help="Example: 22")
  parser.add_argument("ip",help="Example: 192.168.1.1/32")
  parser.add_argument("region",help="Example: us-east-1")
 
@@ -16,7 +16,7 @@ def main():
 
  ec2 = boto3.resource('ec2', region_name=args.region)
  security_group = ec2.SecurityGroup(args.security_group)
- response = security_group.authorize_ingress(IpProtocol="tcp",FromPort=int(args.protocol_number),ToPort=int(args.protocol_number),CidrIp=args.ip)
+ response = security_group.authorize_ingress(IpProtocol="tcp",FromPort=int(args.port_number),ToPort=int(args.port_number),CidrIp=args.ip)
  
  if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
   print "Rule Added"
